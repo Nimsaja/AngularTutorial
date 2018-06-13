b814b216d8ffb8f27285c2bfa7a14f5c50e515db
Nimsaja <jasmin.roeper@nexgo.de>
2018-06-13 13:07:58 +0200

    change heroes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,11 +1,11 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
+  { id: 11, name: 'Mario' },
+  { id: 12, name: 'Alex Megos' },
+  { id: 13, name: 'Adam Ondra' },
+  { id: 14, name: 'Shauna Coxsey' },
+  { id: 15, name: 'Lena Herrmann' },
   { id: 16, name: 'RubberMan' },
   { id: 17, name: 'Dynama' },
   { id: 18, name: 'Dr IQ' },
